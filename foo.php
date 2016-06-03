4d81c25c01060aa046369e262eb1c1c16ef26b3a
Ron-Raz <ron.raz.inc@gmail.com>
2016-06-02 22:47:34 -0400

    my first foomit

---
+++ foo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>my foo!</title>
+  </head>
+  <body>
+    here it is:
+    <?php echo "this is <h1>fooparta!</h1>" ?>
+  </body>
+</html>
